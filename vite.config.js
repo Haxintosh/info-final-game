8a4228c790c566cbc8c62c9db4d5bb46b413c2a9
Haxintosh <92402488+Haxintosh@users.noreply.github.com>
2025-05-24 01:31:32 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/info-final-game/",
+  build: {
+    target: 'esnext'
+  }
   // build: {
   //     rollupOptions: {
   //         input: {
